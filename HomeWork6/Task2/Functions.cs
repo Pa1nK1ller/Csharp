adf9b9f0a95f8d8692a68715dd74e9bc62034530
Konstantin <kostya.300397@gmail.com>
2021-11-29 18:39:56 +0300

    HW6_Сделано только 2 задания

---
+++ HomeWork6/Task2/Functions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace Task2
+{
+    struct Functions
+    {
+        public static double F_Sin(double x)
+        {
+            return Math.Sin(x);
+        }
+        public static double F_x2(double x)
+        {
+            return Math.Pow(x, 2);
+        }
+        public static double F_x3(double x)
+        {
+            return Math.Pow(x, 3);
+        }
+        public static double F_Sqrt(double x)
+        {
+            return Math.Sqrt(x);
+        }
+
+        public static double Load(string fileName, out double[] arr)
+        {
+            FileStream fs = new(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new(fs);
+            double min = double.MaxValue;
+            double d;
+            arr = Array.Empty<double>();
+            Console.WriteLine($"\nДанные из файла: {fileName}");
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                Array.Resize(ref arr, i + 1);
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+                arr[i] = d;
+            }
+            bw.Close();
+            fs.Close();
+            return min;
+        }
+
+        public static void SaveFunc(Func F, string fileName, double x, double b, double h)
+        {
+            FileStream fs = new(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new(fs);
+            int counter = 0;
+            while (x <= b)
+            {
+                counter++;
+                bw.Write(F(x));
+                x += h;
+            }
+            bw.Close();
+            fs.Close();
+            Console.WriteLine($"\nВ файл записано: {counter} знач., расчетная функция {F.Method.Name}");
+        }
+    }
+}
---
+++ HomeWork6/Task2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text;
+
+namespace Task2
+{
+    public delegate double Func(double x);
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Func[] arrFunc = new Func[]
+            {
+                Functions.F_Sin,
+                Functions.F_x2,
+                Functions.F_x3,
+                Functions.F_Sqrt
+            };
+            int ans;
+
+            Console.WriteLine($"Какую функцию использовать (выберите нужный вариант)?"
+                + $"\n1 - F_Sin (sin x)"
+                + $"\n2 - F_x2 (x^2)"
+                + $"\n3 - F_x3 (x^3)"
+                + $"\n4 - F_Sqrt(√2) ");
+
+            do
+            {
+                Console.WriteLine("Выберите необходимый номер: ");
+                ans = (int)CheckNumber();
+                if (ans >= arrFunc.Length || ans <= 0)
+                {
+                    Console.WriteLine($"Функция с кодом \"{ans}\" отсутствует в программе");
+                    ans = 0;
+                }
+            } while (ans == 0);
+            Console.WriteLine("Введите минимальное значение аргумента (x): ");
+            double ansX = CheckNumber();
+            double ansSegment;
+            do
+            {
+                Console.WriteLine("Введите отрезок исследования функции (x2): ");
+                ansSegment = CheckNumber();
+                if (ansSegment <= ansX)
+                {
+                    Console.WriteLine($"Правый край интервал \"{ansSegment}\" не может быть меньше значения аргумента {ansX}");
+                    ansSegment = 0;
+                }
+            } while (ansSegment == 0);
+
+            Functions.SaveFunc(arrFunc[ans - 1], "data.bin", ansX, ansSegment, 0.5);
+            Console.WriteLine($"Минимальное значение: {Functions.Load("data.bin", out double[] arr):F2}");
+            ArrPrint(arr, 8);
+            Console.ReadLine();
+        }
+        static double CheckNumber()
+        {
+            while (true)
+                if (!double.TryParse(Console.ReadLine(), out double x))
+                    Console.Write("Ошибка ввода.\nПожалуйста, повторите ввод: ");
+                else return x;
+        }
+        static public void ArrPrint(double[] arr, int rowLen, int width = 5)
+        {
+            int len = 0;
+            StringBuilder msg = new();
+
+            Console.Write($"Элементы массива ({arr.Length} шт.)");
+            foreach (var item in arr)
+            {
+                if (string.Format("{0:F2}", item).Length > len)
+                    len = string.Format("{0:F2}", item).Length;
+            }
+
+            if (len < width) len = width;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                msg.Clear().ToString();
+                string elLen = msg.Append(' ', len - string.Format("{0:F2}", arr[i]).Length).ToString();
+                if (i % rowLen == 0) Console.Write("\n|");
+                Console.Write($"{elLen}{arr[i]:F2}|");
+            }
+            Console.WriteLine();
+        }
+    }
+}
